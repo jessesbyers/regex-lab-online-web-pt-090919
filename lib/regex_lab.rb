ca2898d927be86da921e302799dc891f61db7ab1
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-18 12:40:35 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,11 +1,7 @@
 require 'pry'
 
 def starts_with_a_vowel?(word)
-  word.each do |first_letter|
-
-binding.pry
-    word.scan(/[aeiou]/)
-  end
+  /aeiou/.match?(word, 1) 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
