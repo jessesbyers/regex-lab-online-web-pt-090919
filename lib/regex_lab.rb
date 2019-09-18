82f69cfff4c147c279c03a4a78b41d02b41a00cc
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-18 12:35:28 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,10 @@
+require 'pry'
+
 def starts_with_a_vowel?(word)
-  word.scan(/[aeiou]/)
+  word.each do |first_letter|
+
+binding.pry
+    word.scan(/[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
