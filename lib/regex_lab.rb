789f656388f5f3dbe3383f90ba173fa75d8aef38
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-18 12:43:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,11 @@
 require 'pry'
 
 def starts_with_a_vowel?(word)
-  /[aeiou]/.match?(word,1)
+  if /[aeiou]/.match?(word,1)
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
